001e1a766af54beabc8eae6507a5efb6bdcad4c5
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2023-01-16 22:57:40 +0100

    Started debugging spmm

--- src/gnn/cl_kernels/aggr.cl
+++ src/gnn/cl_kernels/aggr.cl
@@ -3,14 +3,14 @@ __kernel void aggr (const int vlen,
                     __global const float *A_nnz,
                     __global const int    *A_idx_ptr,
                     __global const int    *A_idx,
-                    __global cosnt float *B,
+                    __global const float *B,
                     __global float *C)
 {
     // Thread identifiers
     const int i = get_global_id(0); // Row ID
     const int j = get_global_id(1); // Col ID
 
-    for (auto e = A_idx_ptr[i]; e < A_idx_ptr[i+1]; e++) {
-        C[i * y + j] += A[A_idx[e]] * B[i * y + j];
+    for (int e = A_idx_ptr[i]; e < A_idx_ptr[i+1]; e++) {
+        C[i * vlen + j] += A_nnz[A_idx[e]] * B[i * vlen + j];
     }
 }
--- src/gnn/tb/aggr/main.cpp
+++ src/gnn/tb/aggr/main.cpp
@@ -1,17 +1,17 @@
 #include "cl_utils.h"
+
 #include <unistd.h>
 #include <stdlib.h>
 
 static bool almost_equal(float a, float b, int ulp = 21);
 static void aggr(const int vlen, const int vnum, const float *A, const int *A_idx_ptr, const int *A_idx, const float *B, float *C);
-static int gen_spm(const int vlen, const int vnum, float *A, int *A_idx_ptr, int *A_idx);
+float * gen_spm(const int vlen, const int vnum, float *A, int *A_idx_ptr, int *A_idx);
 
 int main (int argc, char* argv[]){
 
     //CPU data 
     int vlen = 16;
-    int vnum = 32;
-    int nnz;
+    int vnum = 16;
     float *A;
     float *B = (float*)malloc(vlen*vnum*sizeof(float));
     float *C = (float*)malloc(vlen*vnum*sizeof(float));
@@ -27,15 +27,20 @@ int main (int argc, char* argv[]){
         B[i] = random<float>(-1.0f, 1.0f);
         C[i] = 0.0f;
     }
-    nnz = gen_spm(vlen, vnum, A, A_idx_ptr, A_idx);
+    A = gen_spm(vlen, vnum, A, A_idx_ptr, A_idx);
+    std::cout << "Non zero elements:" << A_idx_ptr[vnum] << std::endl;
+    for (int i = 0; i < vnum; i++)
+        std::cout << "A_idx_ptr[" << i << "]:\t" << A_idx_ptr[i] << "\tA_idx[" << i << "]:\t" << A_idx[i] << std::endl;
+    for (size_t i = 0; i < A_idx_ptr[vnum]; ++i)
+        std::cout << "A[" << i << "]:\t" << A[i] << std::endl;
 
     //Exec OpenCL
     clInit();
-    A_d = clMallocRO(nnz*sizeof(float), true, A);
+    A_d = clMallocRO(A_idx_ptr[vnum]*sizeof(float), true, A);
     B_d = clMallocRO(vlen*vnum*sizeof(float), true, B);
     C_d = clMallocWO(vlen*vnum*sizeof(float), true, C);
     A_idx_ptr_d = clMallocRO((vnum)*sizeof(int), true, A_idx_ptr);
-    A_idx_d = clMallocRO(nnz*sizeof(int), true, A_idx);
+    A_idx_d = clMallocRO(A_idx_ptr[vnum]*sizeof(int), true, A_idx);
     clLoadProgram("./bin/kernels/aggr.pocl","aggr");
     clSetArgs(0, 0, (void *) &vlen, sizeof(int));
     clSetArgs(0, 1, (void *) &vnum, sizeof(int));
@@ -45,10 +50,10 @@ int main (int argc, char* argv[]){
     clSetArgs(0, 5, (void *) &B_d, sizeof(cl_mem));
     clSetArgs(0, 6, (void *) &C_d, sizeof(cl_mem));
     size_t g_work_size[2] = {vnum, vlen};
-    size_t l_work_size[2] = {16, 16};
+    size_t l_work_size[2] = {8, 8};
     clInvokeKernel(0, 2, g_work_size, l_work_size);
     clMemcpyD2H(C_d, vlen*vnum*sizeof(float), (void*) C);
-    clFinish(oclHandle.queue);
+    clFinish(oclHandles.queue);
 
     //Exec CPU
     aggr(vlen, vnum, A, A_idx_ptr, A_idx, B, H);
@@ -106,15 +111,15 @@ static void aggr(const int vlen, const int vnum, const float *A, const int *A_id
     }
 }
 
-static int gen_spm(const int vlen, const int vnum, float *A, int *A_idx_ptr, int *A_idx){
+float * gen_spm(const int vlen, const int vnum, float *A, int *A_idx_ptr, int *A_idx){
     int nnz = 0;
-    int rrr = 0;
+    float rrr = 0.0f;
     for (int i = 0; i < vnum; i++){
         A_idx_ptr[i] = nnz;
-        for (int j = 0; j < vnum; j++){
-            rrr = random<int>( -100, 100);
-            if (abs(rrr) >= 98){
-                A_idx[nnz] = j;
+        for (int j = 0; j < vnum*vnum; j++){
+            rrr = random<float>( -1.0f, 1.0f);
+            if (abs(rrr) >= 0.98f){
+                A_idx[nnz] = nnz;
                 nnz++;
             }
         }
@@ -122,7 +127,7 @@ static int gen_spm(const int vlen, const int vnum, float *A, int *A_idx_ptr, int
     A_idx_ptr[vnum] = nnz;
     A = (float*)malloc(nnz*sizeof(float));
     for (int i = 0; i < nnz; i++){
-        A[i] = random<float>(nnz, -1.0f, 1.0f);
+        A[i] = random<float>(-1.0f, 1.0f);
     }
-    return nnz;
+    return A;
 }
