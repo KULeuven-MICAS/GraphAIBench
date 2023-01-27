5e0e4de6bbf6b00e9bfe2ba88a9d6136ea041a57
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2023-01-27 16:35:43 +0100

    cleaning of useless tests

--- src/gnn/tb/gcn/main.cpp
+++ src/gnn/tb/gcn/main.cpp
@@ -25,8 +25,6 @@ int main (int argc, char * argv []) {
     reader->readGraphFromGRFile(full_graph);
     reader->readGraphFromGRFile(h_full_graph);
     int num_samples = full_graph->size();
-    
-    //num_samples = 32;
 
     std::vector<label_t> dummy_labels(num_samples, 0);
     int num_cls = reader->read_labels(dummy_labels, 0); //fake labels read
@@ -36,7 +34,7 @@ int main (int argc, char * argv []) {
     h_full_graph->add_selfloop();
     
     int dim_init = reader->read_features(input_features);
-    //dim_init = 32;
+
     //construct network here
     int dim_in[layers_size]      = {dim_init, dim_hid};
     int dim_out[layers_size]     = {dim_hid, num_cls};
@@ -73,16 +71,10 @@ int main (int argc, char * argv []) {
     float* d_input_features = (float*)clMallocRW(num_samples * dim_init * sizeof(float));
     clMemcpyH2D((cl_mem)d_input_features, num_samples * dim_init * sizeof(float), input_features.data());
 
-    //generate here partitioning
-    //transfer_data_to_device();
-
     full_graph->compute_vertex_data();
     full_graph->compute_edge_data();
     h_full_graph->compute_vertex_data();
     h_full_graph->compute_edge_data();
-
-    //for (int j=0; j<3; j++) std::cout << "host " << h_full_graph->host_edge_data_ptr()[h_full_graph->host_edge_dst_ptr()[j]] << std::endl;
-    //for (int j=0; j<3; j++) std::cout << "device " <<full_graph->host_edge_data_ptr()[full_graph->host_edge_dst_ptr()[j]] << std::endl;
 
     full_graph->alloc_on_device();
     full_graph->copy_to_gpu();
@@ -113,15 +105,6 @@ int main (int argc, char * argv []) {
     d_feat_in[0] = d_input_features;
     d_feat_in[2] = (float*)clMallocRW(num_samples * num_cls * sizeof(float));
 
-    //DEBUG GPU DATA
-    //float *de;
-    //de = (float*)malloc(h_full_graph->sizeEdges() * sizeof(float));
-    //std::cout << "Limit: " << h_full_graph->row_start_host_ptr()[32] << std::endl;
-    //clMemcpyD2H((cl_mem)full_graph->edge_data_ptr(), h_full_graph->sizeEdges() * sizeof(float), de);
-    ////for (int j=0; j<h_full_graph->sizeEdges(); j++) if (de[h_full_graph->host_edge_dst_ptr()[j]]!= h_full_graph->host_edge_data_ptr()[h_full_graph->host_edge_dst_ptr()[j]]) std::cout << "ERROR " << j << std::endl;
-    //for (int j=4; j<8; j++) std::cout << "copied " << de[h_full_graph->edge_dst_host_ptr()[j]] << std::endl;
-    //for (int j=4; j<8; j++) std::cout << "host " << h_full_graph->edge_data_host_ptr()[h_full_graph->edge_dst_host_ptr()[j]] << std::endl;
-
     //CPU computation
     for (int i = 0; i < layers_size; i++) {
         if (dim_in[i] > dim_out[i]) {
@@ -133,28 +116,6 @@ int main (int argc, char * argv []) {
             matmul(num_samples, dim_out[i], dim_in[i], out_temp[i], W_neigh[i], feat_in[i+1]); // x*y; y*z; x*z
         }
     }
-
-    //DEBUG GPU DATA
-    //std::cout << "DEBUG GPU DATA" << std::endl;
-    //float *din, *dW;
-    //din = (float*)malloc(num_samples * dim_in[0] * sizeof(float));
-    //dW = (float*)malloc(dim_in[0] * dim_out[0] * sizeof(float));
-    //clMemcpyD2H((cl_mem)d_feat_in[0], num_samples * dim_in[0] * sizeof(float), din);
-    //clMemcpyD2H((cl_mem)d_W_neigh[0], dim_in[0] * dim_out[0] * sizeof(float), dW);
-    ////for (int j = 0; j < 32; j++) std::cout << "\tA:" << din[j] << "\tj:" << j << "\tB:" << dW[j*dim_out[0]] << "\tk:" << j*dim_out[0] <<std::endl;
-    //for (int j = 0; j < 32; j++) std::cout << "\tA:" << din[j] << "\tB:" << dW[j*dim_out[0]] << std::endl;
-
-    //DEBUG CPU DATA
-    //std::cout << "DEBUG CPU matmul" << std::endl;
-    //for (int j = 0; j < 32; j++) std::cout << "\tA:" << feat_in[0][j] << "\tB:" << W_neigh[0][j*dim_out[0]] << std::endl;
-    //std::cout << " C:" << out_temp[0][0] << std::endl;
-
-    //for(int i = 0; i < dim_in[0]*dim_out[0]; i++){
-    //    if (i%dim_in[0] == 0) std::cout << std::endl;
-    //    std::cout << dW[i] << " ";
-    //}
-    //std::cout << std::endl;
-    //std::cout << "B:" << std::endl;
 
     //GPU computation
     //forward_layer 
@@ -194,17 +155,6 @@ int main (int argc, char * argv []) {
                         (cl_mem) d_feat_in[i+1]); // x*y; y*z; x*z
         }
     }
-
-    //for (int i = 0; i < dim_out[0]; i++) {
-    //    std::cout << "IN: " << out_temp[0][i] << "\tOUT: " << feat_in[1][i] << std::endl;
-    //}
-    //for (int i = 0; i<h_full_graph->host_row_start_ptr()[1]; i++) {
-    //    std::cout << "EDGES: " << h_full_graph->host_edge_data_ptr()[h_full_graph->host_edge_dst_ptr()[i]] << std::endl;
-    //}
-
-    
-    //for (int j=0; j<3; j++) std::cout << "device " << full_graph->host_edge_data_ptr()[full_graph->host_edge_dst_ptr()[j]] << std::endl;
-    //for (int j=0; j<3; j++) std::cout << "pointers " << h_full_graph->host_edge_dst_ptr()[j] << std::endl;
 
     clFinish(oclHandles.queue);
     //checkup
@@ -288,14 +238,11 @@ int main (int argc, char * argv []) {
     free(feat_in[layers_size]);
     clRelease();
     for (size_t i = 0; i < layers_size; i++){
-        std::cout << "Freeing " << i << std::endl;
         clFree((cl_mem)d_W_neigh[i]);
         clFree((cl_mem)d_feat_in[i]);
         clFree((cl_mem)d_out_temp[i]);
     }
-    std::cout << "Done" << std::endl;
     clFree((cl_mem)d_feat_in[layers_size]);
-    std::cout << "Done" << std::endl;
     return 0;
 }
 
@@ -307,18 +254,11 @@ static void aggr(const int vnum, const int vlen, const float *A, const int *A_id
     vec_t neighbor(vlen);
     for (int i = 0; i < vnum; i++){
         for (int j = A_idx_ptr[i]; j < A_idx_ptr[i+1]; j++){
-            //if (j==0 && i==1) std::cout << "EDGE: " << A_idx_ptr[i+1] - A_idx_ptr[i] << std::endl;
-            for (int k = 0; k < vlen; k++){
+            for (int k = 0; k < vlen; k++)
                 neighbor[k] = B[i*vlen+k] * A[A_idx[j]];
-                //if (i==1 && k==0) std::cout << "B: " << B[i*vlen+k] << " A: " << A[A_idx[j]] << " N:" << neighbor[k] << std::endl;
-            }
-            for (int k = 0; k < vlen; k++){
+            for (int k = 0; k < vlen; k++)
                 C[i*vlen+k] += neighbor[k];
-                //if (i==1 && k==0) std::cout << "C: " << C[i*vlen+k] << " N:" << neighbor[k] << std::endl;
-            }
         }
-    //std::cout << "C: " << C[0] << std::endl;
-    //throw("CRASH");
     }
 }
 
